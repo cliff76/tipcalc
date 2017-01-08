01abd9123a6c7d91982c7bfbc3d75baf177677e8
Angela Andrews <angela@aandrew.com>
2017-01-08 13:47:07 -0500

    Tip Calc works, split check is still in progress

--- scripts.js
+++ scripts.js
@@ -1,10 +1,12 @@
 function tipCalculate (slider, bill){
-  var tip = document.getElementById('tipamount');
-  var slideval = document.getElementById('slideval');
-  var bill = document.getElementById(bill).value;
-  var prcnt = slider * .01;
-  
-  
+    
+    var tip = document.getElementById('tipamount');
+    var total = document.getElementById('billAmount');
+    var slideval = document.getElementById('slideval');
+    var bill = parseInt(document.getElementById(bill).value);
+ 
+    var prcnt = slider * .01;
+ 
   if (bill == null || bill == '') {
     tip.innerHTML = 'Please enter an amount';
     return false;
@@ -13,8 +15,34 @@ function tipCalculate (slider, bill){
     tip.innerHTML = 'Please enter a number';
     return false;
   }
+    
+        
   if(bill >= 0){
-    tip.innerHTML = '$' + (bill * prcnt) .toFixed(2);
-  slideval.innerHTML = slider + '%';
+    tipToLeave = (bill * prcnt);
+      tip.innerHTML = '$' + (bill * prcnt) .toFixed(2);
+        slideval.innerHTML = slider + '%';
+        total.innerHTML = '$' + (bill + tipToLeave).toFixed(2);
   }
+    
 }
+
+
+function splitCheck(bill){
+    var bill = parseInt(document.getElementById(bill).value);
+    var split = document.getElementById('splitCheck');
+    var pp = document.getElementById('perPerson');
+    
+     if (bill == null || bill == '') {
+    pp.innerHTML = 'Please enter an amount';
+    return false;
+  }
+  if(isNaN(bill)) {
+    pp.innerHTML = 'Please enter a number';
+    return false;
+  }
+    if (split =='' || split >=2) {
+     pp.innerHTML ='$' + ((bill * prcnt) / split).toFixed(2);
+        
+    }
+  }
+
